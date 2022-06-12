620a97b2fbd5e7ec0252dd61a13c2c2404585cd0
Administrator <lizhesystem@126.com>
2022-06-12 23:04:09 +0800

    Initial commit

---
+++ pretty-security-jwt/src/main/java/com/lz/pretty/prettysecurityjwt/PrettySecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.lz.pretty.prettysecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrettySecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrettySecurityJwtApplication.class, args);
+    }
+
+}
---
+++ pretty-security-jwt/src/test/java/com/lz/pretty/prettysecurityjwt/PrettySecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lz.pretty.prettysecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrettySecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
